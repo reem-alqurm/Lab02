16af2fd3a2b0bae064aa9ae00f51bbacc5ff3c4d
Your-Full-Name <r.alqerm@gmail.com>
2021-02-08 22:34:50 +0200

    new eidtes

---
+++ js/app.js
@@ -0,0 +1,69 @@
+
+'use strict';
+
+
+let userName = prompt("What\'s Your Name ?? ") 
+
+      alert("Hi  "+ userName + " ,would you please answer these qustionse about Reem with Y/N:) ");
+
+    let firQ = prompt("Is Reem 27 years old ??")
+    
+      if(firQ.toLowerCase() === "y" || firQ.toLowerCase() === "yes")
+      {
+        alert("yes you are correct");
+        //console.log("For the first Q you are correct");
+      }
+      else if (firQ.toLowerCase() === "n" || firQ.toLowerCase() === "no")
+      {
+        alert("No, thats not correct !");
+        //console.log("For the first Q you are not correct");
+      }
+
+    let secQ = prompt("Did Reem ever want to Turky ??")
+      
+      if(secQ.toLowerCase() === "y" || secQ.toLowerCase() === "yes")
+      {
+        alert("yes you are correct");
+        //console.log("For the Secound Q you are correct")
+      }
+      else if (secQ.toLowerCase() === "n" || secQ.toLowerCase() === "no")
+      {
+        alert("No, thats not correct !");
+        //console.log("For the Secound Q you are not correct")
+      }
+
+    let thirQ = prompt("Does Reem like moveis ??")
+      if(thirQ.toLowerCase() === "y" || thirQ.toLowerCase() === "yes")
+      {
+        alert("yes you are correct");
+        //console.log("For the third Q you are correct");
+      }
+      else if (thirQ.toLowerCase() === "n" || thirQ.toLowerCase() === "no")
+      {
+        alert("No, thats not correct !");
+        //console.log("For the third Q you are not correct");
+      }
+    let fortQ = prompt("Does Reem like Shishbarak ??")
+      if(fortQ.toLowerCase() === "y" || fortQ.toLowerCase() === "yes")
+      {
+        alert("No, thats not correct !");
+        //console.log("For the forth Q you are not correct");
+      }
+      else if (fortQ.toLowerCase() === "n" || fortQ.toLowerCase() === "no")
+      {
+        alert("yes you are correct");
+        //console.log("For the forth Q you are not correct");
+
+      }
+
+      let fiftQ = prompt("Does Reem's favorite move is The Shining ??")
+      if(fiftQ.toLowerCase() === "y" || fiftQ.toLowerCase() === "yes")
+      {
+        alert("yes you are correct");
+        //console.log("For the fifth Q you are correct");
+      }
+      else if (fiftQ.toLowerCase() === "n" || fiftQ.toLowerCase() === "no")
+      {
+        alert("No, thats not correct !");
+        //console.log("For the fifth Q you are not correct");
+      }
